1c231b5c92d4c3707ff733f7daab9be21feb73e1
AshokKoda <a.koda59@gmail.com>
2022-05-02 15:57:54 +0530

    Added:Multiple rides its will return invoice summary

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -3,17 +3,26 @@ package com.bridgelabz.cabinvoicegenerator;
 public class CabInvoiceGenerator {
 
 	private final double COST_PER_KM = 10.0;
-    private final int COST_PER_MIN = 1;
-    private final double MIN_FARE = 5.0;
-    
-    public double calculateFare(double distance, int time) {
-        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
-        return Math.max(totalFare, MIN_FARE);
-    }
-    
+	private final int COST_PER_MIN = 1;
+	private final double MIN_FARE = 5.0;
+
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+		return Math.max(totalFare, MIN_FARE);
+	}
+
+	public InvoiceSummary calculateFare(Rides[] rides) {
+		double totalFare = 0.0;
+		for (Rides ride : rides) {
+			totalFare += calculateFare(ride.getDistance(), ride.getTime());
+		}
+		System.out.println("Length is " + rides.length + " Fare is " + totalFare);
+		return new InvoiceSummary(rides.length, totalFare);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("****************Cab Invoice Generator****************");
-		
+
 		CabInvoiceGenerator cab = new CabInvoiceGenerator();
 		double res = cab.calculateFare(2.0, 5);
 		System.out.println("Total Fare = " + res);
--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -28,4 +28,13 @@ public class InvoiceServiceTest {
 		System.out.println("Total Fare = " + totalFare);
 		Assert.assertEquals(5, totalFare, 0.0);
 	}
+
+	@Test
+	public void givenMultipleRides_whenCalculateFare_ShouldReturnAggregateTotalFare() {
+		CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+		Rides[] rides = { new Rides(2.0, 5), new Rides(1.0, 5), new Rides(0.1, 1) };
+		InvoiceSummary expectedSummary = new InvoiceSummary(3, 45);
+		InvoiceSummary summary = invoiceService.calculateFare(rides);
+		Assert.assertEquals(expectedSummary, summary);
+	}
 }
