471d2836543e63e7be91190576010b6650a269aa
AshokKoda <a.koda59@gmail.com>
2022-05-02 15:35:08 +0530

    Cab Invoice Generator

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenetor.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class CabInvoiceGenetor {
+
+	public static void main(String[] args) {
+		System.out.println("****************Cab Invoice Generator****************");
+
+	}
+
+}
